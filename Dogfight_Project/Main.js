4b5da458c6847852ed0c858b3ef857bcfdfd2ca5
mhulcy <mhulcy@LOAN-770510>
2020-05-29 15:33:42 -0400

    gameState

--- Dogfight_Project/Main.js
+++ Dogfight_Project/Main.js
@@ -1,4 +1,5 @@
-﻿
+﻿const gameState = {}
+
 var config = {
     type: Phaser.AUTO,
     width: 1000,
@@ -20,21 +21,23 @@ function preload() {
     //this.load.image('sky', 'assets/skies/space3.png');
     this.load.image('player', 'images/US_p40.png');
 	this.load.image('enemy', 'images/JAP_a6m.png'); //comment
-	this.load.image('cloud', 'images/anime-clouds-png-2-transparent.png');
+    this.load.image('cloud', 'images/anime-clouds-png-2-transparent.png');
+    this.load.image('wheel', 'images/wheel.png')
 }
 
 function create() {
     //this.add.image(400, 300, 'sky');
 
-    var player = this.add.image(500, 800, 'player');
-    var enemy1 = this.add.image(600, 100, 'enemy');
-	var enemy2 = this.add.image(400, 100, 'enemy');
+    gameState.player = this.add.image(500, 800, 'player');
+    gameState.enemy1 = this.add.image(600, 100, 'enemy');
+	gameState.enemy2 = this.add.image(400, 100, 'enemy');
+    //var wheel = this.add.image(800, 800, 'wheel');
 
 	this.add.image(500, 500, 'cloud');
 
 
-    enemy1.angle = 180
-    enemy2.angle = 180
+    gameState.enemy1.angle = 180
+    gameState.enemy2.angle = 180
 
     
 }
