553c0cee192fd59eef1619b4b16f2ba217ff7758
Garrett-Freddo <gafreddo@gmail.com>
2020-05-30 11:24:00 -0500

    Actually added outline

--- Dogfight_Project/Plane.js
+++ Dogfight_Project/Plane.js
@@ -1,3 +1,60 @@
-class plane {
-    
-}
+var BootScene = new Phaser.Class({
+ 
+    Extends: Phaser.Scene,
+ 
+    initialize:
+ 
+    function BootScene ()
+    {
+        Phaser.Scene.call(this, { key: 'BootScene' });
+    },
+ 
+    preload: function ()
+    {
+        // load the resources here
+    },
+ 
+    create: function ()
+    {
+        this.scene.start('WorldScene');
+    }
+});
+ 
+var WorldScene = new Phaser.Class({
+ 
+    Extends: Phaser.Scene,
+ 
+    initialize:
+ 
+    function WorldScene ()
+    {
+        Phaser.Scene.call(this, { key: 'WorldScene' });
+    },
+    preload: function ()
+    {
+        
+    },
+    create: function ()
+    {
+        // create your world here
+    }
+});
+ 
+var config = {
+    type: Phaser.AUTO,
+    parent: 'content',
+    width: 320,
+    height: 240,
+    zoom: 2,
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 }
+        }
+    },
+    scene: [
+        BootScene,
+        WorldScene
+    ]
+};
